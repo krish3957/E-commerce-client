d71787a165bedb09309be2e8dd960981e79a7f62
krish3957 <kpparekh81602@gmail.com>
2023-12-21 14:32:59 +0530

    Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Announcement from '../components/Announcement';
 import Slider from '../components/Slider';
-import Categories from '../components/Categories';
+// import Categories from '../components/Categories';
 import Products from '../components/Products';
 import NewsLetter from '../components/NewsLetter';
 import Footer from '../components/Footer';
